d4db858e3f155f6695a4bf92a7e3cad83dd670e3
John Hawthorn <john.hawthorn@gmail.com>
2015-10-21 20:07:15 -0700

    Scroll more

--- config.h
+++ config.h
@@ -160,8 +160,8 @@ static MouseShortcut mshortcuts[] = {
 
 static MouseKey mkeys[] = {
 	/* button               mask            function        argument */
-	{ Button4,              XK_NO_MOD,      kscrollup,      { .i = 1 } },
-	{ Button5,              XK_NO_MOD,      kscrolldown,    { .i = 1 } },
+	{ Button4,              XK_NO_MOD,      kscrollup,      { .i = 5 } },
+	{ Button5,              XK_NO_MOD,      kscrolldown,    { .i = 5 } },
 };
 
 /* Internal keyboard shortcuts. */
